b0c4328986bd5188d0ce52e11f87a04c7bb4413e
amdiggs <amdiggs@ucdavis.edu>
2022-12-29 14:41:25 -0800

    Initial Commit

---
+++ SimViz/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimViz
+//
+//  Created by Andrew Diggs on 12/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
